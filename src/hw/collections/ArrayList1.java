b8e96d4a04cbb7f2b21284d91dba05345dbf07dc
rambkarm <rafaildolinskiy@gmail.com>
2023-03-29 18:12:00 +0300

    Initial commita

---
+++ src/hw/collections/ArrayList1.java
@@ -0,0 +1,15 @@
+package hw.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayList1 {
+    public static void main(String[] args) {
+        List<String> colors = new ArrayList<String>();
+        colors.add("розовый");
+        colors.add("голубой");
+        colors.add("белый");
+        colors.add("черный");
+        System.out.println(colors);
+    }
+}
